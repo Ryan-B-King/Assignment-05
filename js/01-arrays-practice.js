732bbb6545004f948a3a2a3a26be5a45fb28ce4f
Rockharrd <33941531+Rockharrd@users.noreply.github.com>
2020-03-05 21:08:09 -0800

    updated assignment

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,24 +1,211 @@
 //STEP 1
 
+// let favoriteMovies = ['Big', 'Jurassic Park', 'Moulin Rouge', 'Avengers: End Game', 'Star Trek: First Contact']
+
+// window.console.log(favoriteMovies[1]);
+
 //STEP 2
+
+// let movies = new Array(5);
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+
+// window.console.log(movies[0]);
 
 //STEP 3
 
+// let movies = new Array(5);
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+// movies[5] = 'To Wong Foo';
+
+// window.console.log(movies.length);
+
 //STEP 4
+
+// let movies = [];
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+
+// delete movies[0];
+
+// window.console.log(movies);
 
 //STEP 5
 
+// let i;
+
+// let movies = [];
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+// movies[5] = 'To Wong Foo';
+// movies[6] = 'Saving Private Ryan';
+
+// for(i = 0; i < movies.length; i++){
+//     window.console.log(movies[i]);
+// };
+
 //STEP 6
+
+// let movie;
+
+// let movies = [];
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+// movies[5] = 'To Wong Foo';
+// movies[6] = 'Saving Private Ryan';
+
+// for(movie in movies) {
+//     window.console.log(movies[movie]);
+// };
 
 //STEP 7
 
+// let movie;
+
+// let movies = [];
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+// movies[5] = 'To Wong Foo';
+// movies[6] = 'Saving Private Ryan';
+
+// for(movie in movies) {
+//     movies.sort();
+//     window.console.log(movies[movie]);
+// };
+
 //STEP 8
+
+// let movie;
+// let leastFav;
+
+// let movies = [];
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+// movies[5] = 'To Wong Foo';
+// movies[6] = 'Saving Private Ryan';
+
+// let leastFavMovies = [];
+// leastFavMovies[0] = 'Too Fast Too Furious';
+// leastFavMovies[1] = 'The Lion King (Remake)';
+// leastFavMovies[2] = 'Glitter';
+
+
+// window.console.log('Movies I like:' + '\n\n');
+
+// for(movie in movies) {
+//     window.console.log(movies[movie]);
+// };
+
+// window.console.log('\n\nMovies I regret watching:\n\n');
+
+// for(leastFav in leastFavMovies) {
+//     window.console.log(leastFavMovies[leastFav]);
+// };
 
 //STEP 9
 
+// let movie;
+// let leastFav;
+
+// let movies = [];
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+// movies[5] = 'To Wong Foo';
+// movies[6] = 'Saving Private Ryan';
+
+// let leastFavMovies = [];
+// leastFavMovies[0] = 'Too Fast Too Furious';
+// leastFavMovies[1] = 'The Lion King (Remake)';
+// leastFavMovies[2] = 'Glitter';
+
+// movies = movies.concat(leastFavMovies);
+
+// movies.sort();
+// movies.reverse();
+
+// for (movie in movies) {
+//     window.console.log(movies[movie]);
+// };
+
 //STEP 10
 
+// let movie;
+// let leastFav;
+
+// let movies = [];
+// movies[0] = 'Big';
+// movies[1] = 'Jurassic Park';
+// movies[2] = 'Moulin Rouge';
+// movies[3] = 'Avengers: End Game';
+// movies[4] = 'Star Trek: First Contact';
+// movies[5] = 'To Wong Foo';
+// movies[6] = 'Saving Private Ryan';
+
+// let leastFavMovies = [];
+// leastFavMovies[0] = 'Too Fast Too Furious';
+// leastFavMovies[1] = 'The Lion King (Remake)';
+// leastFavMovies[2] = 'Glitter';
+
+// movies = movies.concat(leastFavMovies);
+
+// movies.sort();
+// movies.reverse();
+
+// movies = movies.pop();
+// window.console.log(movies);
+
 //STEP 11
+
+let movie;
+let leastFav;
+
+let movies = [];
+movies[0] = 'Big';
+movies[1] = 'Jurassic Park';
+movies[2] = 'Moulin Rouge';
+movies[3] = 'Avengers: End Game';
+movies[4] = 'Star Trek: First Contact';
+movies[5] = 'To Wong Foo';
+movies[6] = 'Saving Private Ryan';
+
+let leastFavMovies = [];
+leastFavMovies[0] = 'Too Fast Too Furious';
+leastFavMovies[1] = 'The Lion King (Remake)';
+leastFavMovies[2] = 'Glitter';
+
+movies = movies.concat(leastFavMovies);
+
+movies.sort();
+movies.reverse();
+
+window.console.log(lastMovie);
+
+
 
 //STEP 12
 
