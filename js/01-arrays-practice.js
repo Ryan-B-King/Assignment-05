682e5b9f6becc3bacd111bca744bbd435d5b51b4
KING-PC\King <ryan13king@gmail.com>
2020-03-12 01:19:12 -0700

    completed pt1

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -242,38 +242,190 @@
 // };
 
 //STEP 13
-let employee1;
-let employee2;
-let employees;
 
-employee1 = [];
-employee2 = [];
-employees = employee1.concat(employee2);
+// let employee1;
+// let employee2;
+// let employees;
 
-employee1['employeeID'] =
-employee1['name'] = 
-employee1['title'] =
-employee1['department'] =
-employee1['employment'] =
+// employee1 = [];
+// employee2 = [];
+// employees = [];
 
-employee2['employeeID'] =
-employee2['name'] = 
-employee2['title'] =
-employee2['department'] =
-employee2['employment'] =
+// employees.push(employee1);
+// employees.push(employee2);
 
-window.console.log(employee[1], 'name'); //display 2nd employee name
+// employee1['employeeID'] = 101;
+// employee1['name'] = 'Amanda Bently';
+// employee1['title'] = 'Manager';
+// employee1['department'] = 'Research and Development';
+// employee1['employment'] = true;
+
+// employee2['employeeID'] = 102;
+// employee2['name'] = 'Charlie Davis';
+// employee2['title'] = 'Assistant';
+// employee2['department'] = 'Marketing';
+// employee2['employment'] = true;
+
+// window.console.log(employees[1]['name']);
 
 //STEP 14
 
+// let employee1;
+// let employee2;
+// let employees;
+// let i;
+
+// employee1 = [];
+// employee2 = [];
+// employees = [];
+
+// employees.push(employee1);
+// employees.push(employee2);
+
+// employee1['employeeID'] = 101;
+// employee1['name'] = 'Amanda Bently';
+// employee1['title'] = 'Manager';
+// employee1['department'] = 'Research and Development';
+// employee1['employment'] = true;
+
+// employee2['employeeID'] = 102;
+// employee2['name'] = 'Charlie Davis';
+// employee2['title'] = 'Assistant';
+// employee2['department'] = 'Marketing';
+// employee2['employment'] = true;
+
+// for (i = 0; 1 < employees.length; i++){
+// window.console.log(employees[i]['name']);
+// };
+
 //STEP 15
+
+// let employee1;
+// let employee2;
+// let employee3;
+// let employees;
+// let i;
+
+// employee1 = [];
+// employee2 = [];
+// employee3 = [];
+// employees = [];
+
+// employees.push(employee1);
+// employees.push(employee2);
+// employees.push(employee3);
+
+// employee1['employeeID'] = 101;
+// employee1['name'] = 'Amanda Bently';
+// employee1['title'] = 'Manager';
+// employee1['department'] = 'Research and Development';
+// employee1['employment'] = true;
+
+// employee2['employeeID'] = 102;
+// employee2['name'] = 'Charlie Davis';
+// employee2['title'] = 'Assistant';
+// employee2['department'] = 'Marketing';
+// employee2['employment'] = true;
+
+// employee3['employeeID'] = 103;
+// employee3['name'] = 'Eric Franks';
+// employee3['title'] = 'Director';
+// employee3['department'] = 'Finance';
+// employee3['employment'] = false;
+
+// for (i = 0; i < employees.length; i++){
+//     if (employees[i]['employment']) {
+//         window.console.log(employees[i]['name']);
+//     } else {
+//         break;
+//     };
+// };
 
 //STEP 16
 
+// let movies = [];
+// let movieNames = [];
+// let i;
+
+// movies = [['Jurassic Park', 1], ['Moulin Rouge', 2], ['Avengers: End Game', 3], ['Too Wong Foo', 4], ['Star Trek: First Contact', 5]];
+
+//     // window.console.log(movies);
+
+// for (i = 0; i < movies.length; i++){
+//     // window.console.log(movies[i][0]);
+
+//     movieNames = movies[i].filter((movie) => {
+//         return typeof movie === 'string';
+//     });
+//     window.console.log(movieNames);
+// };
+
 //STEP 17
+
+// let employees;
+// let i;
+// employees = ['Zak', 'Jessica', 'Mark', 'Fred', 'Sally'];
+
+// showEmployee = (names) => {
+//     window.console.log('Employees: \n\n');
+//     for (i = 0; i < employees.length; i++) {    
+//         window.console.log(names[i].toString() + '\n');
+//     };
+// };
+// showEmployee(employees);
 
 //STEP 18
 
+// let values;
+// let filteredValues;
+// let i;
+
+// values = [58, '', 'abcd', true, null, false, 0];
+
+// filterValues = (value) => {
+//     'use strict'
+//     filteredValues = value.filter((item) => {
+//         if (item) {
+//             return filteredValues = item;
+//         } else {
+//             return;
+//         }
+//     });
+//     window.console.log(filteredValues);
+// };
+// filterValues(values);
+
 //STEP 19
 
+// let numbers;
+// let random;
+
+// numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// randomGenerator = (numericArray) => {
+//     random = parseInt(Math.random() * 10);
+//     window.console.log(numericArray[random]);
+// };
+
+// randomGenerator(numbers);
+
 //STEP 20
+
+// let numbers;
+// let largestValue;
+// let i;
+
+// numbers = [3, 16, 1002, 243, 567, 189, 999, 867];
+
+// largestNumber = (numericArray) => {
+//     largestValue = numericArray[0];
+//     for (i = 0; i < numericArray.length; i++) {
+//         if (largestValue >= numericArray[i]) {
+//             largestValue;
+//         } else if (largestValue <= numericArray[i]) {
+//             largestValue = numericArray[i];
+//         }
+//     };
+//     window.console.log(largestValue);
+// };
+// largestNumber(numbers);
